f43a91e0fe02b0fdf402404d5a317423f73c7d89
Jeffrey Chan <jeffschan97@gmail.com>
2016-05-15 16:33:27 -0700

    Fixed my app by comparing my code to the training repo.

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -2,37 +2,38 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var transparentBg = require('../styles').transparentBg;
 
-// functional stateless component since it's only rendering UI
-function Promp (props) {
-    return (
-        <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-            <h1>{props.header}</h1>
-            <div className="col-sm-12">
-                <form onSubmit={props.onSubmitUser}>
-                    <div className="form-group">
-                        <input  className="form-control" 
-                                placeholder="Github Username"
-                                onChange={props.onUpdateUser}
-                                value={props.username}
-                                type="text" />
-                    </div>
-                    <div className="form-group col-sm-4 col-sm-offset-4">
-                        <button className="btn btn-block btn-success" type="submit">
-                            Continue
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+function Prompt (props) {
+  return (
+    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+      <h1>{props.header}</h1>
+      <div className="col-sm-12">
+        <form onSubmit={props.onSubmitUser}>
+          <div className="form-group">
+            <input
+              className='form-control'
+              onChange={props.onUpdateUser}
+              placeholder='Github Username'
+              type='text'
+              value={props.username} />
+          </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-success"
+              type="submit">
+                Continue
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 Prompt.propTypes = {
-    // header must be a string. useful when working with large teams
-    header: PropTypes.string.isRequired,
-    onUpdateUser: PropTypes.func.isRequired,
-    onSubmitUser: PropType.func.isRequired,
-    username: PropType.string.isRequired
+  onSubmitUser: PropTypes.func.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  header: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
 }
 
-module.exports = Prompt;
+module.exports = Prompt
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,54 +1,48 @@
 var React = require('react');
-var Promp = require('../components/Prompt');
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
-    contextTypes: {
-        router: React.PropTypes.object.isRequired
-    },
-    
-    getInitialState: function() {
-        return {
-            username: ''
-        }
-    },
-    
-    handleUpdateUser: function (e) {
-      this.setState({
-          username: e.target.value
-      })  
-    },
-    
-    handleSubmitUser: function (e) {
-        e.preventDefault();
-        var username = this.state.username;
-        this.setState({
-            username: ''
-        });
-        
-        if (this.props.routeParams.plaeryOne) {
-            // go to /battle
-            this.context.router.push({
-                pathname: '/battle',
-                query: {
-                    playerOne: this.props.routeParams.playerOne,
-                    playerTwo: this.state.username
-                }
-            })
-        } else {
-            this.context.router.push('/playerTwo/' + this.state.username)
-        }
-    },
-    
-    render: function () {
-        // no longer in charge of rendering UI, handles all the logic instead
-        return (
-            <Prompt 
-            onSubmitUser={this.handleSubmitUser} 
-            onUpdateUser={this.handleUpdateUser} 
-            header={this.props.route.header}
-            username={this.state.username} />
-        )
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
     }
+  },
+  handleSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username,
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+  handleUpdateUser: function (event) {
+    this.setState({
+      username: event.target.value
+    });
+  },
+  render: function () {
+    return (
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
+    )
+  }
 });
-    
+
 module.exports = PromptContainer;
