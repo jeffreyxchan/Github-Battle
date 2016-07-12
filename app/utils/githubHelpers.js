fbff051bf0a6d451ac593932f5df44958ccbffca
Jeffrey Chan <jeffschan97@gmail.com>
2016-07-11 23:07:28 -0700

    Added the ability to get information from github. Made a file that would retrieve information from github and pass it over to the ConfirmBattle component. For now, the ConfirmBattle component just waits for data, then pukes that data out once it gets it.

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,29 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id" + id + "&client_secrets=" + sec;
+
+function getUserInfo (username) {
+    return axios.get('https://api.github.com/users/' + username + param)
+}
+
+var helpers = {
+    getPlayersInfo: function (players) {
+        // fetch some data from Github
+        // axios.all takes in array of promises
+        // resolves promise after information for all usernames obtained
+        // returns new array with new values that we wanted
+        return axios.all(players.map(function (username) {
+            return getUserInfo(username)
+        })).then(function (info) {
+            return info.map(function (user) {
+                return user.data;
+            })
+        }).catch(function (err) {
+            console.warn('Error in getPlayersInfo', err)
+        })
+    }
+};
+
+module.exports = helpers;
